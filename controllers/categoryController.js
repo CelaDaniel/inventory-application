a242727d69a3e9ede3e9fd3d3cd5b6cdeebf41c7
Dallair220 <lovance777@gmail.com>
2024-02-11 14:48:36 +0100

    Create all read-only pages // Implement category and item list and detail views

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -20,12 +20,26 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Display list of all Categories.
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Category list');
+  const allCategories = await Category.find({}).exec();
+  res.render('category_list', { title: 'Category List', category_list: allCategories });
 });
 
 // Display detail page for a specific Category.
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
+  const [category, itemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, 'name description price stock').exec(),
+  ]);
+  if (category === null) {
+    const err = new Error('Category not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.render('category_detail', {
+    title: 'Category Detail',
+    category: category,
+    category_items: itemsInCategory,
+  });
 });
 
 // Display Category create form on GET.
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,12 +3,19 @@ const asyncHandler = require('express-async-handler');
 
 // Display list of all Items.
 exports.item_list = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Item list');
+  const allItems = await Item.find({}, 'name').exec();
+  res.render('item_list', { title: 'Item List', item_list: allItems });
 });
 
 // Display detail page for a specific Item.
 exports.item_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Item detail: ${req.params.id}`);
+  const item = await Item.findById(req.params.id).populate('category').exec();
+  if (item === null) {
+    const err = new Error('Item not found');
+    err.status = 404;
+    return next(err);
+  }
+  res.render('item_detail', { item: item });
 });
 
 // Display Item create form on GET.
