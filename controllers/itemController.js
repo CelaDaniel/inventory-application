c15b358b21b515d12007fbc512d6538ece50a80c
Dallair220 <lovance777@gmail.com>
2024-02-13 13:10:48 +0100

    Add form processing for category and item creation

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,8 @@
 const Item = require('../models/item');
+const Category = require('../models/category');
+
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all Items.
 exports.item_list = asyncHandler(async (req, res, next) => {
@@ -20,13 +23,47 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 
 // Display Item create form on GET.
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Item create GET');
+  const categories = await Category.find({}).exec();
+  res.render('item_form', { title: 'Create Item', categories: categories });
 });
 
 // Handle Item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Item create POST');
-});
+exports.item_create_post = [
+  // Validate and sanitize fields.
+  body('name', 'Item name required').trim().isLength({ min: 1 }).escape(),
+  body('description', 'Item description required').trim().isLength({ min: 1 }).escape(),
+  body('price', 'Item price required').trim().isLength({ min: 1 }).escape(),
+  body('stock', 'Item stock required').trim().isLength({ min: 1 }).escape(),
+  body('category.*').escape(),
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+    // Create an item object with escaped and trimmed data.
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      stock: req.body.stock,
+      category: req.body.category,
+    });
+    if (!errors.isEmpty()) {
+      const categories = await Category.find({}).exec();
+      // There are errors. Render form again with sanitized values/error messages.
+      res.render('item_form', {
+        title: 'Create Item',
+        item: item,
+        categories: categories,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid. Save item.
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
 
 // Display Item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
